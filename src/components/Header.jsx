d319d362e1f6c2605fe9118b80fb64ceed0fe3af
Ankur Sharma <ankursharma1493@gmail.com>
2024-01-09 00:43:35 +0530

    header final touches used correct svg's

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,27 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="w-[calc(100vw-224px)] bg-white flex justify-between h-[50px]  ml-[224px] pl-[35px] pr-[45px] py-[32px]">
+    <div className="w-[calc(100vw-224px)] bg-white flex items-center justify-between h-[50px]  ml-[224px] pl-[35px] pr-[45px] py-[32px]">
       <div className='flex'>
-        <div>Payments</div>
-        <div>How it Works</div>
+        <div className='text-[20px] font-normal'>Payouts</div>
+        <div className='text-[14px] flex font-thin ml-[20px] justify-center cursor-pointer items-center'>
+          <img src='./images/help.svg' className='h-[14px] w-[14px] mr-[5px]' />
+          How it Works
+        </div>
+
       </div>
-      <div>
-        <input type="text" placeholder="Search features, tutorials, etc." />
+      <div className='bg-[#F2F2F2] py-[9px] px-[16px] flex items-center w-[400px] rounded-[6px]'>
+        <img src='./images/search.svg' className='h-[16px] w-[16px] mr-[9px]' />
+        <input type="text" placeholder="Search features, tutorials, etc." className='bg-[#F2F2F2] w-full h-full outline-none  border-none' />
       </div>
       <div className='flex'>
-        <img src='./images/leftNavImages/home.png' className='w-8 h-8' />
-        <img src='./images/leftNavImages/home.png' className='w-8 h-8' />
+        <div className='w-[40px] h-[40px] flex justify-center items-center rounded-[100px] bg-[#E6E6E6] cursor-pointer mr-[15px]'>
+          <img src='./images/group.svg' />
+        </div>
+        <div className='w-[40px] h-[40px] flex justify-center items-center rounded-[100px] bg-[#E6E6E6] cursor-pointer'>
+          <img src='./images/menu.svg' />
+        </div>
+
       </div>
     </div>
   );
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Pagination = ({ totalPages }) => {
   return (
     <div className='text-center flex'>
-      <button className='border-[1px] w-[100px] h-[25px] rounded border-cyan-950' >← Prev</button>
+      <button className='border-[1px] w-[80px] h-[25px] rounded border-cyan-950  hover:bg-cyan-100 duration-500' >← Prev</button>
       <div className='w-[350p]'>
         {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
           <button key={page} className={`h-[25px] w-[20px] mx-[6px] rounded-[3px] text-[16px] ${page === 2 ? "bg-blue-500 text-white" : ""}`}>
@@ -12,7 +12,7 @@ const Pagination = ({ totalPages }) => {
           </button>
         ))}
       </div>
-      <button className='border-[1px] w-[100px] h-[25px] rounded border-cyan-950'>Next→</button>
+      <button className='border-[1px] w-[80px] h-[25px] rounded border-cyan-950 hover:bg-cyan-100 duration-500'>Next →</button>
     </div>
   );
 };
